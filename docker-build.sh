83eec59ad25ea2bc103efe46fc4416508f6b2b86
wuzy <beat4ocean@gmail.com>
2025-11-12 12:48:53 +0800

    improve:添加mysql docker service

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-docker build -t jiandaoyundatasyncsystem:2.0.0 -f Dockerfile .
-docker save -o jiandaoyundatasyncsystem_2.0.0.tar jiandaoyundatasyncsystem:2.0.0
-tar -zcvf jiandaoyundatasyncsystem_2.0.0.tar.gz jiandaoyundatasyncsystem_2.0.0.tar && rm -f jiandaoyundatasyncsystem_2.0.0.tar
+echo "Building jiandaoyundatasyncsystem docker image..."
+docker build -t jiandaoyundatasyncsystem:2.1.0 -f Dockerfile .
+docker save -o jiandaoyundatasyncsystem_2.1.0.tar jiandaoyundatasyncsystem:2.1.0
+tar -zcvf jiandaoyundatasyncsystem_2.1.0.tar.gz jiandaoyundatasyncsystem_2.1.0.tar && rm -f jiandaoyundatasyncsystem_2.1.0.tar
+echo "Building jiandaoyundatasyncsystem docker image done"
 
-# # 解压和加载
-# tar -zxvf jiandaoyundatasyncsystem_2.0.0.tar.gz && rm -f jiandaoyundatasyncsystem_2.0.0.tar.gz
-# docker load -i jiandaoyundatasyncsystem_2.0.0.tar
+## 解压和加载
+#tar -zxvf jiandaoyundatasyncsystem_2.1.0.tar.gz && rm -f jiandaoyundatasyncsystem_2.1.0.tar.gz
+#docker load -i jiandaoyundatasyncsystem_2.1.0.tar
 
+echo "Pull mysql docker image..."
+docker pull mysql:8.0
+docker save -o mysql_8.0.tar mysql:8.0
+tar -zcvf mysql_8.0.tar.gz mysql_8.0.tar && rm -f mysql_8.0.tar
+echo "Pull mysql docker image done"
+
+## 解压和加载
+#tar -zxvf mysql_8.0.tar.gz && rm -f mysql_8.0.tar.gz
+#docker load -i mysql_8.0.tar
+
+echo "Clean docker..."
 echo "Y" | docker system prune
+echo "Clean docker done"
