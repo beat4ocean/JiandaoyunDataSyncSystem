e3cc9fa6c095f4d58a58d5e603a2fe89804e3825
wuzy <beat4ocean@gmail.com>
2025-11-14 09:06:22 +0800

    release:2.1.1

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Building jiandaoyundatasyncsystem docker image..."
-docker build -t jiandaoyundatasyncsystem:2.1.0 -f Dockerfile .
-docker save -o jiandaoyundatasyncsystem_2.1.0.tar jiandaoyundatasyncsystem:2.1.0
-tar -zcvf jiandaoyundatasyncsystem_2.1.0.tar.gz jiandaoyundatasyncsystem_2.1.0.tar && rm -f jiandaoyundatasyncsystem_2.1.0.tar
+docker build -t jiandaoyundatasyncsystem:2.1.1 -f Dockerfile .
+docker save -o jiandaoyundatasyncsystem_2.1.1.tar jiandaoyundatasyncsystem:2.1.1
+tar -zcvf jiandaoyundatasyncsystem_2.1.1.tar.gz jiandaoyundatasyncsystem_2.1.1.tar && rm -f jiandaoyundatasyncsystem_2.1.1.tar
 echo "Building jiandaoyundatasyncsystem docker image done"
 
 ## 解压和加载
-#tar -zxvf jiandaoyundatasyncsystem_2.1.0.tar.gz && rm -f jiandaoyundatasyncsystem_2.1.0.tar.gz
-#docker load -i jiandaoyundatasyncsystem_2.1.0.tar
+#tar -zxvf jiandaoyundatasyncsystem_2.1.1.tar.gz && rm -f jiandaoyundatasyncsystem_2.1.1.tar.gz
+#docker load -i jiandaoyundatasyncsystem_2.1.1.tar
 
 echo "Pull mysql docker image..."
 docker pull mysql:8.0
